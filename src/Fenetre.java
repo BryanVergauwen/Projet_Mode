fc4e124ac064c0ab1773ba6c15e833819f7d10ef
Bryan Vergauwen <bryan.vergauwen@hotmail.fr>
2014-10-06 01:54:34 +0200

    update

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -18,15 +18,14 @@ public class Fenetre extends JFrame{
 		setLocationRelativeTo(null);
 		listePoint = new LinkedList<Point>();
 		listePoint.add(new Point(5, 5, 0));
-		listePoint.add(new Point(0, 1, 0));
-		listePoint.add(new Point(10, 10, 1));
+		listePoint.add(new Point(0, 0, 0));
+		listePoint.add(new Point(10, 5, 1));
 	}
 	public void paint(Graphics g){
 		if(listePoint != null){
-			g.setFont(new Font("Arial", Font.PLAIN, 50));
 			g.setColor(Color.black);
 			for(Point p : listePoint)
-				g.fillOval(p.getX() * 20, p.getY() * 20, 50, 50);
+				g.fillOval(30 + p.getX() * 20, 50 + p.getY() * 20, 10, 10);
 		}
 	}
 }
