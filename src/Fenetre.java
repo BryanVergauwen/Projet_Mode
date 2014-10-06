792d90f7a3efd9c65f6c6ef1653dfd329f925266
Bryan Vergauwen <bryan.vergauwen@hotmail.fr>
2014-10-06 03:02:48 +0200

    Base ok

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -1,13 +1,12 @@
 import java.awt.Color;
 import java.awt.Graphics;
-import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.JFrame;
 
 public class Fenetre extends JFrame{
 	private static final long serialVersionUID = 1L;
-	private List<Point> listePoint;
+	private List<Point> listePoint = new GtsReader().getListPoint();
 	
 	public Fenetre(){
 		super("Fenetre_Test");
@@ -15,15 +14,11 @@ public class Fenetre extends JFrame{
 		setSize(800, 600);
 		setBackground(Color.white);
 		setLocationRelativeTo(null);
-		listePoint = new LinkedList<Point>();
-		listePoint.add(new Point(5, 5, 0));
-		listePoint.add(new Point(0, 0, 0));
-		listePoint.add(new Point(10, 5, 1));
 	}
 	public void paint(Graphics g){
 		if(listePoint != null){
 			for(Point p : listePoint)
-				g.fillOval(30 + p.getX() * 20, 50 + p.getY() * 20, 10, 10);
+				g.fillOval(30 + p.getX() * 40, 50 + p.getY() * 40, 10, 10);
 		}
 	}
 }
--- src/GtsReader.java
+++ src/GtsReader.java
@@ -6,33 +6,35 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class GtsReader {
-
 	private List<Point> list;
-	
+
 	public GtsReader() {
 		list = new LinkedList<Point>();
-		try{
-			InputStream ips=new FileInputStream("ressources/test.gts"); 
-			InputStreamReader ipsr=new InputStreamReader(ips);
-			BufferedReader br=new BufferedReader(ipsr);
-			String ligne=br.readLine();
+		try {
+			InputStream ips = new FileInputStream("ressources/cube.gts");
+			BufferedReader br = new BufferedReader(new InputStreamReader(ips));
+			String ligne = br.readLine();
 			String result[] = ligne.split(" ");
 			int nbPoint = Integer.parseInt(result[0]);
-			int i=0;
-			while ((ligne=br.readLine())!=null || nbPoint>i){
-				if(ligne.charAt(0)!='#') {
+			int i = 0;
+			
+			while (ligne != null && i < nbPoint) {
+				if (ligne.charAt(0) != '#') {
 					result = ligne.split(" ");
-					list.add(new Point(Integer.parseInt(result[0]), Integer.parseInt(result[1]), Integer.parseInt(result[2])));
+					int a = Integer.parseInt(result[0]);
+					int b = Integer.parseInt(result[1]);
+					int c = Integer.parseInt(result[2]);
+
+					list.add(new Point(a, b, c));
 					i++;
 				}
+				ligne = br.readLine();
 			}
-			br.close(); 
-		}		
-		catch (Exception e){
-			System.out.println(e.toString());
+			br.close();
+		} catch (Exception e) {
+			System.err.println(e);
 		}
 	}
-	
 	public List<Point> getListPoint() {
 		return list;
 	}
