699b589f506cc60eb1f1f09ed27b2e3abe1b1cfb
VladimirPutin <antoine-59@hotmail.fr>
2014-12-30 20:26:35 +0100

    Rotation sur Z
    A et E pour faire des barrel roll !

--- src/transformations/Rotation.java
+++ src/transformations/Rotation.java
@@ -9,7 +9,7 @@ import objects.Point;
 public class Rotation {
 	private final String X = "X";
 	private final String Y = "Y";
-	//private final String Z = "Z";
+	private final String Z = "Z";
 	
 	public Rotation(List<Point> points, List<Face> faces, double angle, String axe) {
 		double x = 0, y = 0, z = 0;
@@ -35,17 +35,17 @@ public class Rotation {
 			}
 		}
 		
-		/*if (axe.equalsIgnoreCase(Z)) {
+		if (axe.equalsIgnoreCase(Z)) {
 			for (Point p : points) {
 				x = p.getX();
 				y = p.getY();
 				z = p.getZ();
 
 				p.setX(x * Math.cos(angle) - y * Math.sin(angle));
-				p.setY(y * Math.sin(angle) + x * Math.cos(angle));
+				p.setY(x* Math.sin(angle) + y * Math.cos(angle));
 			}
 		}
-		*/
+		
 		for (Face f : faces){
 			f.normalisation();
 			f.barycentre();
